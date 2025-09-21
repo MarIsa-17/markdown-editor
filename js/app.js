6f6ebfa8a74e01911d4b12630659971317ac0bb5
MarIsa-17 <nunezlopezmarisa90@gmail.com>
2025-09-20 23:46:57 -0500

    Generar Vista Previa

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const fondo = document.querySelector('body')
+fondo.style.backgroundColor ="#eeeee4"
+
+
+const encabezado = document.getElementById('encabezado')
+encabezado.style.backgroundColor ="white"
+encabezado.style.margin = "20px 10px"
+encabezado.style.padding ="10px"
+encabezado.style.borderRadius ="15px"
+
+const titulo = document.getElementById('titulo')
+titulo.style.fontFamily ="Franklin Gothic Medium"
+titulo.style.textAlign ="center"
+titulo.style.color ="#7c7c23"
+
+const cajaBoton = document.getElementById('cajaBoton')
+cajaBoton.style.display="flex"
+cajaBoton.style.justifyContent="end"
+
+const boton = document.getElementById('boton')
+boton.classList.add('animacion')
+boton.style.padding ="8px"
+boton.style. margin ="0 10px 10px 0"
+boton.style.border ="none"
+boton.style.borderRadius ="5px"
+boton.style.color ="#575716"
+boton.style.fontWeight ="700"
+boton.style.boxShadow ="2px 2px 4px #b2b2b1"
+boton.style.cursor ="pointer"
+
+
+
